f7287b5388bf202a9a5e1f60ce00d45666b94d76
dh <dhfromkorea@gmail.com>
2018-05-03 13:21:45 -0400

    add test script

---
+++ src/run.sh
@@ -0,0 +1,3 @@
+python3 main.py --model DDPG --env MountainCarContinuous-v0 --outdir output --total 20000 --train 3000 --test 5 --tfseed 0 --npseed 0 --gymseed 0
+python3 main.py --model NAF --env MountainCarContinuous-v0 --outdir output --total 20000 --train 3000 --test 5 --tfseed 0 --npseed 0 --gymseed 0
+python3 main.py --model iCNN --env MountainCarContinuous-v0 --outdir output --total 20000 --train 3000 --test 5 --tfseed 0 --npseed 0 --gymseed 0
