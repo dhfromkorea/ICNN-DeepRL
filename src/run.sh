5b69232afcabaa70f162434222638c791ef8d51b
dh <dhfromkorea@gmail.com>
2018-05-04 03:06:59 -0400

    run.sh

--- src/run.sh
+++ src/run.sh
@@ -8,11 +8,12 @@ alpha=0.6
 beta0=0.9
 gamma=0.75
 tmax=200
+beta_iters=$tmax*$total_episode
 
 for trial_i in $(seq "$n_trial")
 do
     echo "running ICNN $trial_i"
-    python3 main.py --model ICNN --env $env --outdir output --total_episode 100 --train_episode 5 \
+    python3 main.py --model ICNN --env $env --outdir output --total_episode $total_episode --train_episode 5 \
                     --test_episode 5 --trial_i $trial_i --outheta 0.15 --ousigma 0.2 --icnn_bn $batchnorm \
                     --alpha $alpha --beta0 $beta0 --force true --is_training $is_training \
                     --tmax $tmax --discount $gamma
@@ -23,7 +24,7 @@ python3 archive/plot-single.py --model_path output/ICNN/ --model ICNN --n_trial 
 for trial_i in $(seq "$n_trial")
 do
     echo "running DDPG $trial_i"
-    python3 main.py --model DDPG --env $env --outdir output --total_episode 100 --train_episode 5 \
+    python3 main.py --model DDPG --env $env --outdir output --total_episode $total_episode --train_episode 5 \
                     --test_episode 5 --outheta 0.15 --ousigma 0.2 \
                     --alpha $alpha --beta0 $beta0 --force true --trial_i $trial_i --is_training $is_training \
                     --tmax $tmax --discount $gamma
@@ -35,7 +36,7 @@ python3 archive/plot-single.py --model_path output/DDPG/ --model DDPG --n_trial 
 for trial_i in $(seq "$n_trial")
 do
     echo "running NAF $trial_i"
-    python3 main.py --model NAF --env $env --outdir output --total_episode 100 --train_episode 5 \
+    python3 main.py --model NAF --env $env --outdir output --total_episode $total_episode --train_episode 5 \
                     --test_episode 5 --outheta 0.15 --ousigma 0.2 --naf_bn $batchnorm \
                     --alpha $alpha --beta0 $beta0 --force true --trial_i $trial_i --is_training $is_training \
                     --tmax $tmax --discount $gamma
